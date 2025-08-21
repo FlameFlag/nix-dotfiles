738f72a7079d320c0e841c269847090a5f7332ab
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-08-21 18:07:27 +0300

    chore(cli/direnv): use `pkgsUnstable`

--- modules/hm/cli/direnv.nix
+++ modules/hm/cli/direnv.nix
@@ -1,10 +1,16 @@
-{ lib, config, ... }:
+{
+  pkgsUnstable,
+  lib,
+  config,
+  ...
+}:
 {
   options.hm.direnv.enable = lib.mkEnableOption "Direnv";
 
   config = lib.mkIf config.hm.direnv.enable {
     programs.direnv = {
       enable = true;
+      package = pkgsUnstable.direnv;
       nix-direnv.enable = true;
     };
   };
