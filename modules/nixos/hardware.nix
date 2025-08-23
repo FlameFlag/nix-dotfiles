fc546caa43c0da6f647452c6f5ceb4e879b035b4
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-08-23 23:06:19 +0300

    feat(nixos/hardware): use latest NVIDIA drivers (580.76.05)

--- modules/nixos/hardware.nix
+++ modules/nixos/hardware.nix
@@ -87,7 +87,14 @@
     })
     (lib.mkIf config.nixOS.nvidia.enable {
       hardware = {
-        nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
+        nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "580.76.05";
+          sha256_64bit = "sha256-IZvmNrYJMbAhsujB4O/4hzY8cx+KlAyqh7zAVNBdl/0=";
+          sha256_aarch64 = lib.fakeSha256;
+          openSha256 = "sha256-xEPJ9nskN1kISnSbfBigVaO6Mw03wyHebqQOQmUg/eQ=";
+          settingsSha256 = "sha256-ll7HD7dVPHKUyp5+zvLeNqAb6hCpxfwuSyi+SAXapoQ=";
+          persistencedSha256 = lib.fakeSha256;
+        };
         nvidia.modesetting.enable = true;
         nvidia.powerManagement.enable = true;
 
