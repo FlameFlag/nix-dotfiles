ab908e51a07207b5b4f49146db3e47063c513bf9
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-08-23 23:00:37 +0300

    feat(hm/tui/yazi/): add conditionals for `git`, `starship` plugins

--- modules/hm/tui/yazi/default.nix
+++ modules/hm/tui/yazi/default.nix
@@ -11,6 +11,7 @@
     ./keybindings.nix
     ./settings.nix
   ];
+
   options.hm.yazi.enable = lib.mkEnableOption "Yazi";
 
   config = lib.mkIf config.hm.yazi.enable {
@@ -35,28 +36,29 @@
         {
           diff = "${pluginsRepo}/diff.yazi";
           full-border = "${pluginsRepo}/full-border.yazi";
-          git = "${pluginsRepo}/git.yazi";
           hide-preview = "${pluginsRepo}/hide-preview.yazi";
           max-preview = "${pluginsRepo}/max-preview.yazi";
           smart-enter = "${pluginsRepo}/smart-enter.yazi";
-          starship = pkgs.fetchFromGitHub {
-            owner = "Rolv-Apneseth";
-            repo = "starship.yazi";
-            rev = "a63550b2f91f0553cc545fd8081a03810bc41bc0";
-            hash = "sha256-PYeR6fiWDbUMpJbTFSkM57FzmCbsB4W4IXXe25wLncg=";
-          };
           system-clipboard = pkgs.fetchFromGitHub {
             owner = "orhnk";
             repo = "system-clipboard.yazi";
             rev = "4f6942dd5f0e143586ab347d82dfd6c1f7f9c894";
             hash = "sha256-M7zKUlLcQA3ihpCAZyOkAy/SzLu31eqHGLkCSQPX1dY=";
           };
+        }
+        // lib.optionalAttrs config.programs.git.enable { git = "${pluginsRepo}/git.yazi"; }
+        // lib.optionalAttrs config.programs.starship.enable {
+          starship = pkgs.fetchFromGitHub {
+            owner = "Rolv-Apneseth";
+            repo = "starship.yazi";
+            rev = "a63550b2f91f0553cc545fd8081a03810bc41bc0";
+            hash = "sha256-PYeR6fiWDbUMpJbTFSkM57FzmCbsB4W4IXXe25wLncg=";
+          };
         };
-      initLua = ''
-        require('full-border'):setup()
-        require("git"):setup()
-        require("starship"):setup()
-      '';
+      initLua =
+        ''require('full-border'):setup()''
+        + lib.optionalString config.programs.git.enable ''require("git"):setup()''
+        + lib.optionalString config.programs.starship.enable ''require("starship"):setup()'';
     };
   };
 }
