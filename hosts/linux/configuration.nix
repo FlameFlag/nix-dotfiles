374c56a7c6414dac1203fa2aa4a91b7bdbe8c1df
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-08-26 02:21:48 +0300

    Revert "feat(hosts/linux/configuration): use latest linux kernel"
    This reverts commit c4a8d05e990df164bf474a5604e50e2278cb3a8c.

--- hosts/linux/configuration.nix
+++ hosts/linux/configuration.nix
@@ -51,7 +51,6 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest; # Depends on NVIDIA 580 or higher
 
   time.timeZone = "Europe/Sofia";
 
