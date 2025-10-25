c86b3dadd11eabd46df02a2bad0455078aab5c5e
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-10-25 20:58:47 +0300

    feat(helix): build from `d79cce4...` (2025-10-25)

--- modules/common/nixpkgs.nix
+++ modules/common/nixpkgs.nix
@@ -4,6 +4,7 @@
     config.allowUnfree = true;
     overlays = [
       inputs.nur.overlays.default
+      inputs.helix.overlays.default
       (final: prev: {
         yt-dlp = final.callPackage ../../pkgs/yt-dlp.nix { };
         yt-dlp-script = final.callPackage ../../pkgs/yt-dlp-script.nix { };
