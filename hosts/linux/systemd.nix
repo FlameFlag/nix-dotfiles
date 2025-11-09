e29f9f1e232424a60283d98fb49c03305136aa0f
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-11-09 23:36:48 +0200

    fix(lenovo): battery systemd service

--- hosts/linux/systemd.nix
+++ hosts/linux/systemd.nix
@@ -5,15 +5,16 @@ _: {
       conserveModeEnable = {
         description = "Enable Lenovo Conservation Mode";
         enable = true;
+        serviceConfig.Type = "oneshot";
         script = "echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode";
-        wantedBy = [ "default.target" ];
+        wantedBy = [ "multi-user.target" ];
       };
       # Disable "Conserve" Mode and let battery charge to 100%;
       conserveModeDisable = {
         description = "Disable Lenovo Conservation Mode";
-        enable = true;
+        enable = false;
+        serviceConfig.Type = "oneshot";
         script = "echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode";
-        wantedBy = [ "default.target" ];
       };
     };
   };
