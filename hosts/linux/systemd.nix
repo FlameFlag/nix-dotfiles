0794baeeeaa45aa87add2c1e2229a159132c654e
FlameFlag <github@flameflag.dev>
2025-12-27 02:31:10 +0200

    refactor(systemd): toggle conservation mode

--- hosts/linux/systemd.nix
+++ hosts/linux/systemd.nix
@@ -1,20 +1,23 @@
 _: {
   systemd = {
     services = {
-      # Set 60% Charging limit to "conserve" battery life
-      conserveModeEnable = {
-        description = "Enable Lenovo Conservation Mode";
+      lenovo-con-mode = {
+        description = "Toggle Lenovo Conservation Mode (60% ↔ 100%)";
         enable = true;
         serviceConfig.Type = "oneshot";
-        script = "echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode";
+        script = ''
+          CURRENT=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
+          if [ "$CURRENT" = "1" ]; then
+            echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
+            echo "Conservation Mode: DISABLED (100% charge)"
+          else
+            echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
+            echo "Conservation Mode: ENABLED (60% charge)"
+          fi
+        '';
+        startLimitBurst = 5;
+        startLimitIntervalSec = 10;
         wantedBy = [ "multi-user.target" ];
-      };
-      # Disable "Conserve" Mode and let battery charge to 100%;
-      conserveModeDisable = {
-        description = "Disable Lenovo Conservation Mode";
-        enable = false;
-        serviceConfig.Type = "oneshot";
-        script = "echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode";
       };
     };
   };
