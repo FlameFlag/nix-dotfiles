992a1ced736808b2af72922d69dc361a1241e0e0
FlameFlag <github@flameflag.dev>
2026-01-19 15:56:19 +0200

    style(launchd): nest all under `launchd.user.agents`

--- modules/darwin/launchd.nix
+++ modules/darwin/launchd.nix
@@ -5,44 +5,44 @@
   ...
 }:
 {
-  launchd.user.agents."symlink-zsh-config" = {
-    script = ''
-      for file in zprofile zshenv zshrc; do
-        ln -sfn "/etc/''${file}" "/Users/${config.system.primaryUser}/.''${file}"
-      done
-    '';
-    serviceConfig.RunAtLoad = true;
-    serviceConfig.StartInterval = 0;
-  };
+  launchd.user.agents = {
+    symlink-zsh-config = {
+      script = ''
+        for file in zprofile zshenv zshrc; do
+          ln -sfn "/etc/''${file}" "/Users/${config.system.primaryUser}/.''${file}"
+        done
+      '';
+      serviceConfig.RunAtLoad = true;
+      serviceConfig.StartInterval = 0;
+    };
 
-  launchd.user.agents."zero-capslock-delay".serviceConfig = {
-    ProgramArguments = [
-      "/usr/bin/hidutil"
-      "property"
-      "--set"
-      "{\"CapsLockDelayOverride\":0}"
-    ];
-    RunAtLoad = true;
-    StartInterval = 0;
-  };
+    zero-capslock-delay.serviceConfig = {
+      ProgramArguments = [
+        "/usr/bin/hidutil"
+        "property"
+        "--set"
+        "{\"CapsLockDelayOverride\":0}"
+      ];
+      RunAtLoad = true;
+      StartInterval = 0;
+    };
 
-  launchd.user.agents."raycast-ai-openrouter-proxy".serviceConfig = {
-    ProgramArguments = [
-      "/bin/sh"
-      "-c"
-      ''
-        export API_KEY=$(cat ${config.sops.secrets.raycast-openrouter-api-key.path});
-        export PORT=11435;
-        export BASE_URL=https://openrouter.ai/api/v1;
-        exec ${pkgs.raycast-ai-openrouter-proxy}/bin/raycast-ai-openrouter-proxy
-      ''
-    ];
-    RunAtLoad = true;
-    KeepAlive = true;
-  };
+    raycast-ai-openrouter-proxy.serviceConfig = {
+      ProgramArguments = [
+        "/bin/sh"
+        "-c"
+        ''
+          export API_KEY=$(cat ${config.sops.secrets.raycast-openrouter-api-key.path});
+          export PORT=11435;
+          export BASE_URL=https://openrouter.ai/api/v1;
+          exec ${lib.getExe' pkgs.raycast-ai-openrouter-proxy "raycast-ai-openrouter-proxy"}
+        ''
+      ];
+      RunAtLoad = true;
+      KeepAlive = true;
+    };
 
-  launchd.user.agents.atuin-daemon = {
-    serviceConfig = {
+    atuin-daemon.serviceConfig = {
       ProgramArguments = [
         (lib.getExe' pkgs.unstable.atuin "atuin")
         "daemon"
