bb35986b1b29cde6b759c0cae09b37849a4feaf8
FlameFlag <github@flameflag.dev>
2026-04-03 02:07:21 +0300

    feat(nix): add flox binary cache

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -25,10 +25,12 @@
         substituters = [
           "https://devenv.cachix.org"
           "https://nix-community.cachix.org"
+          "https://cache.flox.dev"
         ];
         trusted-public-keys = [
           "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
         ];
       }
       // lib.optionalAttrs config.nixpkgs.hostPlatform.isLinux { flake-registry = ""; };
