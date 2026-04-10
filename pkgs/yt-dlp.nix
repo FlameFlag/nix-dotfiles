473fb0ff02d29d45fe69cca7aa47d55230c170ce
FlameFlag <github@flameflag.dev>
2026-04-10 11:51:37 +0300

    chore: bump

--- pkgs/yt-dlp.nix
+++ pkgs/yt-dlp.nix
@@ -1,12 +1,12 @@
 { yt-dlp, fetchFromGitHub }:
 
 yt-dlp.overridePythonAttrs (old: {
-  version = "2026.03.17-unstable-2026-03-28";
+  version = "2026.03.17-unstable-2026-04-07";
 
   src = fetchFromGitHub {
     owner = "yt-dlp";
     repo = "yt-dlp";
-    rev = "87eaf886f5a1fed00639baf3677ac76281cd98f9";
-    hash = "sha256-wD143mYXY/0mXSYawL6ZRC4SGawSX05Z20FrBlLNkXE=";
+    rev = "a4acc4223289eeb4d32af7b798bfe6e9c38f4b8d";
+    hash = "sha256-rmMiMZV7T51PVp/OXeh6J9KAqeM/NkoRhh9GWLpbQTM=";
   };
 })
