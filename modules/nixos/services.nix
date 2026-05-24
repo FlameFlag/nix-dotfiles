6d6f9e2cf3c401c2d37b4d87d4145e3d9168d755
FlameFlag <git@ps1.sh>
2026-05-24 17:59:34 +0300

    feat(nixos): enable kmscon console

--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -1,5 +1,27 @@
-_: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  paidFonts = (pkgs.callPackage ../../pkgs/paid-fonts/build-font.nix { }).packages;
+in
+{
   services = {
+    kmscon = {
+      enable = true;
+      hwRender = true;
+      useXkbConfig = true;
+      extraOptions = "--term xterm-256color";
+      fonts = lib.mkIf config.flame.fonts.paid.enable [
+        {
+          name = "TX-02 Nerd Font";
+          package = paidFonts.tx-02;
+        }
+      ];
+    };
+
     libinput.enable = true;
     openssh.enable = true;
     xserver.xkb.layout = "us";
