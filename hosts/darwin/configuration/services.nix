a322bf9ffa96b592af98802a57f851606aa78bcb
FlameFlag <git@ps1.sh>
2026-06-07 21:19:35 +0300

    feat(darwin): enable Tailscale SSH

--- hosts/darwin/configuration/services.nix
+++ hosts/darwin/configuration/services.nix
@@ -1,11 +1,19 @@
-{ pkgs, ... }:
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  tailscalePackage = pkgs.unstable.tailscale;
+  tailscaleExe = lib.meta.getExe tailscalePackage;
+in
 {
   services = {
     openssh.enable = true;
 
     tailscale = {
       enable = true;
-      package = pkgs.unstable.tailscale;
+      package = tailscalePackage;
     };
 
     ghidra-mcp = {
@@ -15,6 +23,32 @@
       mcpHost = "127.0.0.1";
       mcpPort = 8090;
       allowScripts = true;
+    };
+  };
+
+  launchd.daemons.tailscale-ssh = {
+    script = ''
+      i=0
+      while [ "$i" -lt 30 ]; do
+        if ${tailscaleExe} set --ssh=true; then
+          exit 0
+        fi
+        i=$((i + 1))
+        sleep 2
+      done
+      exit 1
+    '';
+    serviceConfig = {
+      Label = "com.tailscale.tailscale-ssh";
+      RunAtLoad = true;
+      KeepAlive = {
+        Crashed = true;
+        SuccessfulExit = false;
+      };
+      ThrottleInterval = 30;
+      ProcessType = "Background";
+      StandardOutPath = "/var/log/tailscale-ssh.log";
+      StandardErrorPath = "/var/log/tailscale-ssh.log";
     };
   };
 }
